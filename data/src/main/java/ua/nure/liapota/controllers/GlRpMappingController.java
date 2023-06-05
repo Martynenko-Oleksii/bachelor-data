9f578249b9263dca30ec58db12a8f4163292350d
OlehLiapota <oleh.liapota@nure.ua>
2023-06-05 19:15:50 +0300

    glPrMapping

--- data/src/main/java/ua/nure/liapota/controllers/GlRpMappingController.java
+++ data/src/main/java/ua/nure/liapota/controllers/GlRpMappingController.java
@@ -4,15 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ua.nure.liapota.annotations.Authorize;
 import ua.nure.liapota.models.data.ValueTypeEntity;
 import ua.nure.liapota.services.GlRpMappingService;
 import ua.nure.liapota.services.ValueTypeService;
 
 import java.util.List;
 
-@RestController("/glRpMapping")
+@Authorize("data,gl-pr-mapping")
+@RestController
+@RequestMapping("/glPrMapping")
+@CrossOrigin(origins = "http://localhost:4200")
 public class GlRpMappingController {
     private final GlRpMappingService service;
     private final ValueTypeService valueTypeService;
